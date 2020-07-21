b3d6b8572a666bb2633f793b2141bf5fbf5635a5
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 21:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(timeString){
-  
+  return (timeString.>)
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(greet){
