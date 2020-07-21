2b926b21303e254f6f7d4c3cf29b4c52af9269d0
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 21:56:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(timeString){
   return (timeString.parseInt(timeString,10)<12)?"Good Morning":
-   (timeString.parseInt(timeString,10)<12)?
+   (timeString.parseInt(timeString,10)>12 && timeString.parseInt(timeString,10)<17 )?
+   "Good Afternoon":"Good Evening";
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(greet){
