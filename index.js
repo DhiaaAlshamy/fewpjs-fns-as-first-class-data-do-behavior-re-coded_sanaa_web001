7f39b9c4797d92cb2a6a18a62fb662619212766f
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 22:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function greet(timeString){
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(greet){
-  document.getElementById(greeting).innerText=greet.value;
+  document.getElementById(greeting).innerText=greet;
   
 }
